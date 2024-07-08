89d36940041190e545ae3962a6c5faa96c848e54
minh.nguyen <minh2.nguyen@phoenixunity.io>
2024-07-08 11:54:00 +0700

    Init lib

--- src/labs/Lab1.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export interface LabProps {}
-
-export default function Lab(props: LabProps) {
-  return <div></div>;
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+// Header
+// Introduce about my self
+// Tech stack
+// History working
+// Link contact
+
+function Porfolio() {
+	return <div>ABC</div>;
+}
+
+export default Porfolio;
